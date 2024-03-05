e203f281b8ccb34cf2876f905492ce401dd0ce99
Itobuz <itobuz@Itobuzs-MacBook-Pro.local>
2024-03-05 13:04:41 +0530

    Client Section JS remains

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const heroSliderContainer = document.querySelector('.hero-slider');
-
 const plantImagesSection = document.querySelector('.plant-collection-images');
 const previousButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
 const plantCollectionImages = [{imageName: "bird-paradise", name: "1. Bird of Paradise"}, {imageName: "rubber-plant", name: "2. Rubber Plants"}, {imageName: "string-pearls", name: "3. String of Pearls"}, {imageName: "rubber-plant", name: "4. Rubber Plants"}];
 const heroSliderImages = [{imageName: "main-plant"}, {imageName: "main-plant-copy1"}, {imageName: "main-plant-copy2"}];
+
+function updateHeroCarousel(heroCards, current) {
+    heroCards.forEach(card => {
+        card.style.height = "36rem";
+        card.style.transform = `translateX(-${currentHeroSlide * 100}%)`;
+        card.style.zIndex = 0;
+    });
+    heroCards[current].style.height = "32rem";
+    heroCards[current].style.zIndex = 2;
+}
+
 function plantCollection (plantCollectionImages, container, section){
     plantCollectionImages.forEach(element => {
         const imageSection = document.createElement('div');
@@ -26,7 +36,6 @@ plantCollection(plantCollectionImages, plantImagesSection, "plant-collection");
 previousButton.addEventListener('click', () => {
     plantImagesSection.innerHTML = '';
     const card = plantCollectionImages.shift();
-    console.log(plantCollectionImages.length);
     plantCollectionImages.push(card);
     plantCollection(plantCollectionImages, plantImagesSection, "plant-collection");
 });
@@ -34,12 +43,28 @@ previousButton.addEventListener('click', () => {
 nextButton.addEventListener('click', () => {
     plantImagesSection.innerHTML = '';
     const card = plantCollectionImages.pop()
-    console.log(plantCollectionImages.length);
     plantCollectionImages.unshift(card);
     plantCollection(plantCollectionImages, plantImagesSection, "plant-collection");
 });
 
-
 // HOME SLIDER
 
 plantCollection(heroSliderImages, heroSliderContainer, "home");
+
+const heroCards = document.querySelectorAll('.home-card');
+const heroPreviousButton = document.querySelector('.hero-left-slider-button');
+const heroNextButton = document.querySelector('.hero-right-slider-button');
+const noOfHeroSlides = heroSliderImages.length;
+let currentHeroSlide = 0;
+updateHeroCarousel(heroCards, currentHeroSlide);
+
+heroPreviousButton.addEventListener('click', () => {
+    currentHeroSlide = (currentHeroSlide+1)%noOfHeroSlides;
+    updateHeroCarousel(heroCards, currentHeroSlide);
+});
+
+heroNextButton.addEventListener('click', () => {
+    currentHeroSlide = (currentHeroSlide-1+noOfHeroSlides)%noOfHeroSlides;
+    updateHeroCarousel(heroCards, currentHeroSlide);
+});
+
